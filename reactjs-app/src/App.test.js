0f371d1d87aceb62ad3fa9be47ca0dfe25441a50
Miguel Osuna <mosuna.valdez@gmail.com>
2020-09-27 00:04:44 -0500

    Installed react-dom that was a missing dependency for enzyme and created some initial tests in App component

--- reactjs-app/src/App.test.js
+++ reactjs-app/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme'
 import App from './App';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  it('renders a router', () => {
+    const app = shallow(<App />)
+    expect(app.find(Router).length).toEqual(1)
+  })
+})
+
+describe('<App />', () => {
+  it('renders a switch', () => {
+    const app = shallow(<App />)
+    expect(app.find(Switch).length).toEqual(1)
+  })
+})
