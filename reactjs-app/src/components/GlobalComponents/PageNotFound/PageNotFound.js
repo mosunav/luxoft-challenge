8e3724e434b57d09d991a0d3fd0379cd49cc8e01
Miguel Osuna <mosuna.valdez@gmail.com>
2020-10-02 10:49:48 -0500

    Fixed design for mobile sizes in Details Page view and added responsiveness to Page not found component

--- reactjs-app/src/components/GlobalComponents/PageNotFound/PageNotFound.js
+++ reactjs-app/src/components/GlobalComponents/PageNotFound/PageNotFound.js
@@ -4,7 +4,11 @@ import './PageNotFound.css'
 export const PageNotFound = () => {
   return (
     <div className="page-not-found-container">
-      <img src={window.location.origin + '/img/page-not-found.jpeg'} width="100%" height="100%" alt="Not found" />
+      <img 
+        className={'page-not-found-image'}
+        src={window.location.origin + '/img/page-not-found.jpeg'}
+        alt="Not found"
+      />
     </div>
   )
 }
